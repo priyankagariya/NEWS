930f319df34d6fe07e9fa2a30b25f5b6947457d0
PRIYANKA <priyankagariya18@gmail.com>
2024-08-08 23:42:20 +0530

    script.js

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const newsContainer = document.getElementById('news-container');
 const customImagePath = 'images/img.jpg'; 
 
 async function fetchNews() {
+
+      try {
+          
     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
+
+            if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+          
     const data = await response.json();
     displayNews(data.articles);
+
+            } catch (error) {
+        console.error('Error fetching the news:', error);
+        newsContainer.innerHTML = '<p>Sorry, something went wrong. Please try again later.</p>';
 }
 
 function displayNews(articles) {
